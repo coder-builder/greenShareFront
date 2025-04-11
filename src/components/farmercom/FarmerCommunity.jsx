3da9320736c84ee2f130a83ecab5a79b363a2ab9
PangJin97 <kindred5795@naver.com>
2025-04-11 17:46:35 +0900

    게시글 만들기

--- src/components/farmercom/FarmerCommunity.jsx
+++ src/components/farmercom/FarmerCommunity.jsx
@@ -1,13 +1,34 @@
 import React from "react";
 import styles from "./FarmerCommunity.module.css";
+import { useNavigate } from "react-router-dom";
 
 const FarmerCommunity = () => {
+  const nav = useNavigate()
+
   return (
     <>
-      <h2>자유 게시판</h2>
-
+      <h2>식물 이야기</h2>
       <div className={styles.container}>
-        <div>자유게시판 목록</div>
+       
+        <div className={styles.imgDiv}>
+          <p>이미지</p>
+          <p>작성자: USER_ROLE</p>
+        </div>
+        
+        <div className={styles.contentDiv}>
+          <p>제목: title</p>
+          <p>내용: content</p>
+        </div>
+        
+        <div className={styles.likeDiv}>
+          <p>좋아요 이모지 / 좋아요 갯수</p>
+          <p>댓글 이모지 / 갯수</p>
+        </div>
+
+        <button type="button" onClick={(e)=>{
+          nav('/reg-community')
+        }}>게시글 등록</button>
+      
       </div>
     </>
   );
---
+++ src/components/farmercom/FarmerCommunityInsert.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FarmerCommunityInsert = () => {
+  return (
+    <div>FarmerCommunityInsert</div>
+  )
+}
+
+export default FarmerCommunityInsert
